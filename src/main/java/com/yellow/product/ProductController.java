84ec024422a7f78b5520842b83366c32052f638c
Jay Solanki <j194solanki@gmail.com>
2022-04-13 19:10:48 +0530

    delete product service

--- src/main/java/com/yellow/product/ProductController.java
+++ src/main/java/com/yellow/product/ProductController.java
@@ -1,9 +1,14 @@
 package com.yellow.product;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.yellow.login.User;
+
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/product")
 public class ProductController {
 
@@ -19,5 +24,20 @@ public class ProductController {
     public Iterable<Product> getProducts() {
         return productRepo.findAll();
     }
-
+    
+    @GetMapping("/detail/{productId}")
+    public Optional<Product> getProductDetails(@PathVariable Long productId) {
+        return productRepo.findById(productId);
+    }
+    
+    @GetMapping("/remove/{productId}")
+    public boolean removeProductDetail(@PathVariable Long productId) {
+    	Optional<Product> product=this.getProductDetails(productId);
+    	if(product.isPresent()){
+        	productRepo.delete(product.get());
+        	return true;
+    	}else {    		
+    		return false;
+    	}
+    }
 }
